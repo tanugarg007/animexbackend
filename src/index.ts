d5e0ab676fb207b815eb603946598eb6137b3b4a
tanugarg007 <tanugarg252@gmail.com>
2026-03-02 17:53:12 +0530

    fix vercel build

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,24 @@ import dotenv from 'dotenv';
 import { router } from './router/approuter';
 
 dotenv.config();
+
 const app = express();
+
 const corsOptions = {
-  origin: '*', 
-  methods: '*',        
+  origin: '*',
+  methods: '*',
   allowedHeaders: 'Content-Type,Authorization',
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/users',router);
+
+app.use('/users', router);
 app.use("/uploads", express.static("uploads"));
 
 app.get('/', (req, res) => {
-    res.send('Backend is running!');
+  res.send('Backend is running!');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;   // ✅ IMPORTANT
